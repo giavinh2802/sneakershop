85524875fc8157bad52eb66e9154ef8b19962d5b
nhan2k <nhanrooney113@gmail.com>
2022-03-10 11:48:05 +0700

    10/3 update

--- src/index.js
+++ src/index.js
@@ -2,26 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Assets/Styles/index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-import { createStore } from 'redux'
+import { createStore } from "redux";
 
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 
-import rootReducer from './redux/reducers'
+import rootReducer from "./redux/reducers";
 
-import './Assets/boxicons-2.0.7/css/boxicons.min.css'
-import './Assets/Styles/css/grid.css';
-import './Assets/Styles/css/index.css';
-import './Assets/Styles/css/theme.css';
+import "./Assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./Assets/Styles/css/grid.css";
+import "./Assets/Styles/css/index.css";
+import "./Assets/Styles/css/theme.css";
 
-import Layout from "./Components/Layout/Layout"
+import Layout from "./Components/Layout/Layout";
 
-const store = createStore(
-  rootReducer
-)
+const store = createStore(rootReducer);
 
-document.title = 'Sneaker Shop'
+document.title = "Sneaker Shop";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -29,11 +26,5 @@ ReactDOM.render(
       <Layout />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
